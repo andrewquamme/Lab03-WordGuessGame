60fcd2768c65683bf95a590d1a58bef348200c1d
andrewquamme <andrewquamme@gmail.com>
2019-01-10 11:39:57 -0800

    testing System.IO file manipulation

---
+++ WordGuessGame/WordGuessGame/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace WordGuessGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "../../../../words.txt";
+            if (File.Exists(path))
+            {
+                FileReadWords(path);
+            }
+            else
+            {
+                FileCreateWords(path);
+            }
+            FileAppendWord(path);
+            //FileDeleteWords(path);
+            
+        }
+
+        static void FileCreateWords(string path)
+        {
+            using(StreamWriter streamwriter = new StreamWriter(path))
+            {
+                streamwriter.WriteLine("Dog\nCat\nFerret");
+            }
+        }
+
+        static void FileReadWords(string path)
+        {
+            string[] words = File.ReadAllLines(path);
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+        }
+
+        static void FileAppendWord(string path)
+        {
+            using (StreamWriter streamWriter = File.AppendText(path))
+            {
+                streamWriter.WriteLine(DateTime.UtcNow);
+            }
+        }
+
+        static void FileDeleteWords(string path)
+        {
+            File.Delete(path);
+        }
+
+        static void RandomNumberGenerator()
+        {
+
+        }
+    }
+}
